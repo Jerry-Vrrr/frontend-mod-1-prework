2514ce2bdfe5e564f87e6b410a697a74ad5d4447
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2021-11-02 08:00:44 -0600

    Update concatenation.js

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -6,7 +6,7 @@ One example is already completed. Your task is to complete
 any remaining prompt.
 
 When navigated to the root of this project directory, you should be able to run this
-file from your terminal with the command `node section1/exercises/dataTypes.js`
+file from your terminal with the command `ç`
 */
 
 // EXAMPLE: Write code that combines the variables below into another string:
@@ -22,7 +22,7 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here: "My zoo has 7 unicorns!"
 
 
 // YOU DO:
@@ -30,7 +30,7 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
-
+console.log("The" + speedy + "jumped over the" + slowpoke);
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -39,13 +39,17 @@ var slowPoke = "lazy brown dog";
 
 slowPoke = "tortoise";
 speedy = "hare";
+console.log("In a predictable result, the" + slowpoke + "beat the" + speedy!);
 
 
 // YOU DO:
 // Declare three variables, name/content/data type of your choice. Think carefully about what 
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
+ 
+            
 
+            
 
 //-------------------
 // FINAL CHECK
